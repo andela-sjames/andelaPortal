0048039e2e18720230a09dd78567ac6a74fa06be
Akinjide Bankole <r@akinjide.me>
2018-11-20 02:38:06 +0100

    feat: abstract storage and resources

---
+++ src/data.js
@@ -0,0 +1,110 @@
+var RESOURCES = {
+    "internal": [
+        {
+            "title": "Andela",
+            "link": "https://www.andela.com/"
+        },
+        {
+            "title": "Pulse",
+            "link": "https://pulse.andela.com/"
+        },
+        {
+            "title": "Allocations",
+            "link": "https://allocations.andela.com/"
+        },
+        {
+            "title": "AIS",
+            "link": "http://ais.andela.com/"
+        },
+        {
+            "title": "Helpdesk",
+            "link": "http://help.andela.com/"
+        },
+        {
+            "title": "VOF",
+            "link": "http://vof.andela.com/"
+        },
+        {
+            "title": "LV",
+            "link": "https://lv.andela.com"
+        },
+        {
+            "title": "Zhishi",
+            "link": "http://zhishi.andela.com/dashboard"
+        },
+        {
+            "title": "Lenken",
+            "link": "http://lenken.andela.com"
+        },
+        {
+            "title": "Digest",
+            "link": "https://learning-digest.andela.com"
+        },
+        {
+            "title": "Framework",
+            "link": "https://framework.andela.com/"
+        },
+    ],
+    "external": [
+        {
+            "title": "(S)Intranet",
+            "link": "https://sites.google.com/andela.com/home"
+        },
+        {
+            "title": "Bamboohr",
+            "link": "https://andela.bamboohr.com/"
+        },
+        {
+            "title": "Canvas",
+            "link": "https://andela.instructure.com"
+        },
+        {
+            "title": "Twitter",
+            "link": "https://twitter.com/andela"
+        },
+        {
+            "title": "Freckle",
+            "link": "https://andela.letsfreckle.com"
+        },
+        {
+            "title": "Github",
+            "link": "https://github.com/andela/"
+        },
+        {
+            "title": "Greenhouse",
+            "link": "https://app.greenhouse.io/dashboard"
+        },
+        {
+            "title": "Grovo",
+            "link": "https://andela.grovo.com"
+        },
+        {
+            "title": "Balsamiq",
+            "link": "https://andela.mybalsamiq.com/"
+        },
+        {
+            "title": "Pivotal",
+            "link": "https://www.pivotaltracker.com/dashboard"
+        },
+        {
+            "title": "Pluralsight",
+            "link": "https://www.pluralsight.com/"
+        },
+        {
+            "title": "SI",
+            "link": "https://andela.small-improvements.com/app/home"
+        },
+        {
+            "title": "(L)Intranet",
+            "link": "https://sites.google.com/andela.com/learning"
+        },
+        {
+            "title": "Medium",
+            "link": "https://medium.com/the-Andela-way"
+        },
+        {
+            "title": "Facebook",
+            "link": "https://www.facebook.com/thisisandela"
+        },
+    ]
+};
